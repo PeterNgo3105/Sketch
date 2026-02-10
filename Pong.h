7cfb1b37a5e02f1307570d4b4498a86f8133fd51
Peter Ngo <pheromaria3105@gmail.com>
2026-02-09 23:27:18 -0800

    adding Pong header file

--- Pong.h
+++ Pong.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "stdafx.h"
+#include "CBase4618.h"
+/** @brief This class 
+*
+*
+* @Author Peter Ngo
+*/
+class CPong : CBase4618 {
+public:
+	virtual void update();
+	virtual void draw();
+
+};
